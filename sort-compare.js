e9118d4e49cf87474012c2d00d4f668df2b66360
Dominick Benjamin <dominickalce@gmail.com>
2024-12-15 23:47:53 -0500

    Eliminación de archivos innecesarios y actualización del README

--- sort-compare.js
+++
@@ -1,8 +0,0 @@
-const arr = ["80", "9", "700", 40, 1, 5, 200];
-function comparar(a, b) {
-  return a - b;
-}
-
-console.log("original:", arr.join());
-console.log("ordenado sin función:", arr.sort());
-console.log("ordenado con función:", arr.sort(comparar));
